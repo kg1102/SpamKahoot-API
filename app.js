706bf5be3de593e22065890df6316e91cdd4a4f9
0KaioGomes <kg6370694@gmail.com>
2021-04-29 22:10:55 -0300

    upload archives

---
+++ app.js
@@ -0,0 +1,34 @@
+const express        = require('express');
+const app            = express();
+const port           = process.env.PORT || 444;
+const KahootSpam     = require('kahoot-spam')
+var   api            = KahootSpam
+
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
+app.post('/send', (req,res) => {
+    var { id, nick, amount } = req.body;
+    if(!isNaN(id,amount)){
+        if(id == '' || nick == '' || amount == ''){
+            res.status(400);
+            res.send(JSON.stringify({status:"error", reason: "algum campo está vazio!"}, null, 4));
+        }else{
+            api.spam(id, `${nick}`, amount);
+            res.status(201);
+            res.send(JSON.stringify({status:"success", bots_sended: amount, nickname: nick, amount: amount}, null, 4));
+        }
+    }else{
+        res.status(400);
+        res.send(JSON.stringify({status:"error", reason: "o número de quantidade ou id não são números!"}, null, 4));
+    }
+});
+
+app.listen(port, ()=>{
+    console.log(`[1] Servidor iniciado com sucesso na porta @ `, port);
+});
